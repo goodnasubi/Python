a145dd9161d11a619da4029fada04b170f4a800c
Takashi Ikeda <goodnasubi@gmail.com>
2017-03-16 20:41:56 +0900

    test 2

--- PythonTest/src/odd_or_even.py
+++ PythonTest/src/odd_or_even.py
@@ -1,1 +1,12 @@
 #  http://qiita.com/Usek/items/fba0991e224a1cdea274
+
+def odd_or_even():
+    print("")
+    for i in range(1,6):
+        if i % 2 != 0:
+            print("{0} is odd".format(i))
+        else:
+            print("{0} is even".format(i))
+
+if __name__ == "__main__":
+    odd_or_even()
